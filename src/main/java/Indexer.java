948c120555b2865e2c329fc26b3896515623cfa3
saubhagyasharma99 <92671977+saubhagyasharma99@users.noreply.github.com>
2021-10-19 01:05:27 +0100

    Update Indexer.java

--- src/main/java/Indexer.java
+++ src/main/java/Indexer.java
@@ -1,22 +1,4 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements.  See the NOTICE file distributed with
- * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
- * (the "License"); you may not use this file except in compliance with
- * the License.  You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- *
- * The base source code from the Lucene website was modified as part of my submission for
- * the module 'Information Retrieval and Web Search (CS7IS3)' in Trinity College Dublin.
- */
+
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -43,7 +25,6 @@ import org.apache.lucene.store.FSDirectory;
 
 public class Indexer {
 
-    /** Index all text files under a directory. */
     public void indexMethod() {
         String indexPath = "index";
         String docsPath = "cran/cran.all.1400";
@@ -61,37 +42,21 @@ public class Indexer {
 
             Directory dir = FSDirectory.open(Paths.get(indexPath));
 
-            //Analyzer analyzer = new SimpleAnalyzer();
-            //Analyzer analyzer = new WhitespaceAnalyzer();
-            //Analyzer analyzer = new StandardAnalyzer();
+          
             Analyzer analyzer = new EnglishAnalyzer();
 
             IndexWriterConfig iwc = new IndexWriterConfig(analyzer);
 
-            //BM25 Similarity
+         
             iwc.setSimilarity(new BM25Similarity());
 
-            //Classic Similarity
-            //iwc.setSimilarity(new ClassicSimilarity());
-
-            //LMDirichletSimilarity
-            //iwc.setSimilarity(new LMDirichletSimilarity());
-
-            //Trying a multi similarity model
-            //iwc.setSimilarity(new MultiSimilarity(new Similarity[]{new BM25Similarity(),new ClassicSimilarity()}));
-
-            //Trying another multi similarity model
-            //iwc.setSimilarity(new MultiSimilarity(new Similarity[]{new BM25Similarity(),new LMDirichletSimilarity()}));
-
-            //Trying another multi similarity model
-            //iwc.setSimilarity(new MultiSimilarity(new Similarity[]{new ClassicSimilarity(),new LMDirichletSimilarity()}));
+           
 
             iwc.setOpenMode(OpenMode.CREATE);
 
             IndexWriter writer = new IndexWriter(dir, iwc);
             indexDoc(writer, docDir);
 
-            //Using writer.forceMerge to maximise search performance.
             writer.forceMerge(1);
 
             writer.close();
@@ -104,7 +69,6 @@ public class Indexer {
         }
     }
 
-    /** Indexes the 'cran.all.1400' file */
     static void indexDoc(IndexWriter writer, Path file) throws IOException {
         try (InputStream stream = Files.newInputStream(file)) {
 
@@ -118,10 +82,7 @@ public class Indexer {
             while(currentLine != null){
                 Document doc = new Document();
                 if(currentLine.startsWith(".I")){
-                    /*
-                     * I think the ID of the document does not make sense to be analysed,
-                     * hence it is just directly stored without any analysis.
-                     */
+                   
                     doc.add(new StringField("id", currentLine.substring(3), Field.Store.YES));
                     currentLine = bufferedReader.readLine();
                 }
@@ -149,10 +110,7 @@ public class Indexer {
                         fullText += currentLine + " ";
                         currentLine = bufferedReader.readLine();
                     }
-                    /*
-                     * After a bit of analysis, I found that for this dataset, analysing
-                     * and storing bibliography details proved to be slightly inefficient.
-                     */
+                  
                     doc.add(new StringField("bibliography", fullText, Field.Store.YES));
                     fullText = "";
                 }
@@ -162,7 +120,7 @@ public class Indexer {
                         fullText += currentLine + " ";
                         currentLine = bufferedReader.readLine();
                     }
-                    //Not storing the words in an attempt to save storage space.
+                    
                     doc.add(new TextField("words", fullText, Field.Store.NO));
                     fullText = "";
                 }
